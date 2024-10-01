88940d4d0a027343ce42c991d460f4d56dbc7674
nathanmandell99@gmail.com <nathanmandell99@gmail.com>
2024-10-01 16:58:04 -0400

    Initial Django setup.

---
+++ mm_scheduling/scheduling/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Meeting
+
+# Create your views here.
+
+
+def create_meeting(request):
+    pass
+
+
+def invite_participants(request):
+    pass
