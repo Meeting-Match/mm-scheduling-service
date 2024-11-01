8d7c3d20663280ff2c22ba92f6503f0fe07119ad
Nathan <nathanmandell99@gmail.com>
2024-11-01 13:10:29 -0400

    Add Event model

--- mm_scheduling/scheduling/models.py
+++ mm_scheduling/scheduling/models.py
@@ -3,5 +3,10 @@ from django.db import models
 # Create your models here.
 
 
-class Meeting(models.Model):
-    pass
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    organizer_id = models.IntegerField()  # Single user ID for the organizer
+    participant_ids = models.JSONField(blank=True, default=list)
+    datetime = models.DateTimeField()
+    description = models.TextField(blank=True)
+    location = models.CharField(max_length=255, blank=True)
