40a9af591f40b219c9bd89f4883e1160db135cd2
Nathan <nathanmandell99@gmail.com>
2024-11-01 13:20:55 -0400

    Add Availbility model

--- mm_scheduling/scheduling/models.py
+++ mm_scheduling/scheduling/models.py
@@ -10,3 +10,13 @@ class Event(models.Model):
     datetime = models.DateTimeField()
     description = models.TextField(blank=True)
     location = models.CharField(max_length=255, blank=True)
+
+
+# This is an entry for a the availability of a User, associated with an Event
+class Availability(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    participant_id = models.IntegerField()
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+# We need to set up some sort of integrity check here to make sure start is
+# before end.
