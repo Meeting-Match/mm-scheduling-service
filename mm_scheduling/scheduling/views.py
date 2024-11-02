c4d8f6fec4b03b065b6bf85488e306c9d1d3c6be
Nathan <nathanmandell99@gmail.com>
2024-11-02 16:38:32 -0400

    Refactor views for proper create and read of Event objects

--- mm_scheduling/scheduling/views.py
+++ mm_scheduling/scheduling/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render
 from .models import Event, Availability
 from .serializers import EventSerializer, AvailabilitySerializer
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 # Create your views here.
 
 
+# Event_id needs to be taken from the json data
 @api_view(['GET'])
 def get_event(request, event_id):
     event = Event.objects.get(pk=event_id)
     serializer = EventSerializer(event)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def create_event(request):
+    serializer = EventSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data(status=201))
+    else:
+        return Response({"error": "Invalid data"},
+                        status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
