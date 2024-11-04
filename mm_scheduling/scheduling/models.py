f50399314ce1cfdeb56fde94f17198684c1160f7
Nathan <nathanmandell99@gmail.com>
2024-11-04 17:07:40 -0500

    Add access permission for non-GET operations on models.

--- mm_scheduling/scheduling/models.py
+++ mm_scheduling/scheduling/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+from rest_framework.permissions import BasePermission
 
 # Create your models here.
+
+
+class IsOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Check if the user is trying to perform a safe method like GET
+        if request.method in ('GET', 'HEAD', 'OPTIONS'):
+            return True
+        # Only allow updates and deletes if the user is the creator
+        return obj.creator == request.user
 
 
 class Event(models.Model):
@@ -11,15 +21,19 @@ class Event(models.Model):
     datetime = models.DateTimeField()
     description = models.TextField(blank=True)
     location = models.CharField(max_length=255, blank=True)
+    permission_classes = [IsOwner]
 
 
 # This is an entry for the availability of a User, associated with an Event
 class Availability(models.Model):
     event = models.ForeignKey(
-        Event, on_delete=models.CASCADE, related_name="availabilities")
-    participant_id = models.IntegerField()
+        Event, on_delete=models.CASCADE, related_name="availabilities",
+        blank=False)
+    participant_id = models.IntegerField(blank=False)
     start = models.DateTimeField()
     end = models.DateTimeField()
+
+    permission_classes = [IsOwner]
 
     def clean(self):
         super().clean()
