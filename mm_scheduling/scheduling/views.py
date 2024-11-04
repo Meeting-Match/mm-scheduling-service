92000c539da89eb97c67cca53464cbe449b34c77
Nathan <nathanmandell99@gmail.com>
2024-11-04 17:08:16 -0500

    Add JWT authentication support and make user ID add automatically.

--- mm_scheduling/scheduling/views.py
+++ mm_scheduling/scheduling/views.py
@@ -1,6 +1,9 @@
 from .models import Event, Availability
 from .serializers import EventSerializer, AvailabilitySerializer
 from rest_framework import generics
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 # Create your views here.
 
@@ -10,6 +13,11 @@ from rest_framework import generics
 class EventList(generics.ListCreateAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(organizer_id=self.request.user.id)
 
 
 # This uses RetrieveUpdateDestroyAPIView to give us full CRUD on
@@ -18,6 +26,8 @@ class EventList(generics.ListCreateAPIView):
 class EventDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 # This uses ListCreateAPIView to list all instances of the object
@@ -25,6 +35,16 @@ class EventDetail(generics.RetrieveUpdateDestroyAPIView):
 class AvailabilityList(generics.ListCreateAPIView):
     queryset = Availability.objects.all()
     serializer_class = AvailabilitySerializer
+
+    def perform_create(self, serializer):
+        event = serializer.validated_data['event']
+
+        # Check if the request user is a participant of the event
+        if self.request.user.id not in event.participant_ids:
+            raise PermissionDenied("You are not a participant in this event.")
+
+        # Save the Availability instance if the check passes
+        serializer.save()
 
 
 # This uses RetrieveUpdateDestroyAPIView to give us full CRUD on
